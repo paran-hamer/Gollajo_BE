631794e047ef667cccc59f4c97153d5136a68eb8
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-10-07 02:49:16 +0900

    chore: rds(mysql서버)와 s3서버 연결을 위한 환경설정

---
+++ src/main/java/com/gollajo/GollajoApplication.java
@@ -0,0 +1,13 @@
+package com.gollajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GollajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GollajoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gollajo/GollajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gollajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GollajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

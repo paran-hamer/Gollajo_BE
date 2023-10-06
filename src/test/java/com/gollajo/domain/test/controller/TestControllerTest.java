9281493d9cd677fd4afc25be3bcbd2f5922b5343
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-10-07 03:26:00 +0900

    test: testController 테스트 코드 작성

--- src/test/java/com/gollajo/domain/test/controller/TestControllerTest.java
+++ src/test/java/com/gollajo/domain/test/controller/TestControllerTest.java
@@ -1,7 +1,36 @@
 package com.gollajo.domain.test.controller;
 
+import com.gollajo.domain.test.service.TestService;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(TestControllerTest.class)
 class TestControllerTest {
 
+    @MockBean
+    TestService testService;
+
+    @BeforeEach
+    void setUp(){
+        RestAssuredMockMvc.standaloneSetup(new TestController(testService));
+    }
+
+    @Test
+    @DisplayName("testAPI를 실행하여 성공메세지를 받는다")
+    void test(){
+        //given
+        //when&then
+        RestAssuredMockMvc.given().log().all()
+                .when().get("/test")
+                .then().log().all()
+                .status(HttpStatus.OK);
+
+    }
 }

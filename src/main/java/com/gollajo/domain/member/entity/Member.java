92d76942323be9e34928a58e848d8e4b0d94ae17
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-10-07 17:21:58 +0900

    feat/member : 회원가입하는 API 작성

--- src/main/java/com/gollajo/domain/member/entity/Member.java
+++ src/main/java/com/gollajo/domain/member/entity/Member.java
@@ -5,6 +5,7 @@ import com.gollajo.domain.member.entity.enums.Grade;
 import com.gollajo.domain.member.entity.enums.SocialType;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,13 +17,14 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    //필수정보
     @Column(nullable = false)
     private String email;
 
     @Column(length = 20,nullable = false)
     private String nickname;
 
+    //소셜로그인시
     @Column
     @Enumerated(EnumType.STRING)
     private SocialType socialType;
@@ -30,6 +32,7 @@ public class Member {
     @Column
     private String socialId;
 
+    //자동기입: 기본정보
     @Column(nullable = false)
     private int point;
 
@@ -40,11 +43,35 @@ public class Member {
     @Column(nullable = false)
     private int numOfVoting;
 
+    //추가정보
     @Column
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
     @Column
     private int age;
+    @Builder
+    private Member(
+            final String email,
+            final String nickname,
+            final SocialType socialType,
+            final String socialId,
+            final int point,
+            final Grade grade,
+            final int numOfVoting,
+            final Gender gender,
+            final int age
+                   ){
+        this.email = email;
+        this.nickname = nickname;
+        this.socialType = socialType;
+        this.socialId = socialId;
+        this.point = point;
+        this.grade = grade;
+        this.numOfVoting = numOfVoting;
+        this.gender = gender;
+        this.age = age;
+
+    }
 
 }

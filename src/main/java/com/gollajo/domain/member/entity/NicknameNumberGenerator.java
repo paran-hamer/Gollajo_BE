b8d11aa276dd675d2fd162373da3c66dd09578df
Changmin Han <hcmhcs0@ajou.ac.kr>
2023-10-13 15:33:44 +0900

    feat/member : 멤버의 닉네임숫자 제너레이터 생성

---
+++ src/main/java/com/gollajo/domain/member/entity/NicknameNumberGenerator.java
@@ -0,0 +1,11 @@
+package com.gollajo.domain.member.entity;
+
+public class NicknameNumberGenerator implements NumberGenerator{
+
+    private int number = 0;
+
+    @Override
+    public int generate() {
+        return ++number;
+    }
+}
---
+++ src/main/java/com/gollajo/domain/member/entity/NumberGenerator.java
@@ -0,0 +1,5 @@
+package com.gollajo.domain.member.entity;
+
+public interface NumberGenerator {
+    int generate();
+}
